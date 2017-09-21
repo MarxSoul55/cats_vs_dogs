f58d217c251bdde19362fdb2124ca921f23b8676
MarxSoul55 <wjacademic@gmail.com>
2017-09-21 12:51:07 -0700

    Added option to change the rescale-factor for preprocessing images.

--- src/layers/preprocessing.py
+++ src/layers/preprocessing.py
@@ -8,26 +8,27 @@ import numpy as np
 
 class ImagePreprocessor:
 
-    """Helps preprocess images."""
+    """Helps preprocess images for a classifier."""
 
-    def preprocess_image(self, image):
+    def preprocess_image(self, image, rescale):
         """
         Preprocesses an image for the model.
         Converts image to a 256x256x3, 8-bit LAB representation.
 
         # Parameters
             image (str): Path to the image.
+            rescale (tuple): (width, height) of the resulting image.
         # Returns
-            A preprocessed image (numpy array).
+            A preprocessed image as a numpy-array.
         """
         image = cv2.imread(image)
-        image = cv2.resize(image, (256, 256))
+        image = cv2.resize(image, rescale)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
         image = image.astype('float32')
         image /= 255
         return image
 
-    def preprocess_directory(self, steps, train_dir, order):
+    def preprocess_directory(self, steps, train_dir, order, rescale=(256, 256)):
         """
         Given a directory of subdirectories of classes, preprocesses their contents and yields
         the results along with one-hot labels.
@@ -36,8 +37,9 @@ class ImagePreprocessor:
             steps (int): Preprocess `steps` amount of samples from each class.
             train_dir (str): Path to the directory of classes.
             order (list): Classes; order determines the one-hot label.
+            rescale (tuple): (width, height) that each image will be resized to.
         # Yields
-            Batches of data.
+            Current step (starting from 1) and a data-label pair.
         """
         classes = os.listdir(train_dir)
         cursors = {}
@@ -45,11 +47,11 @@ class ImagePreprocessor:
         for class_ in classes:
             cursors[class_] = 0
             images[class_] = sorted(os.listdir(train_dir + '/' + class_))
-        for _ in range(steps):
+        for step in range(steps):
             for class_ in classes:
                 absolute_path = os.path.abspath(train_dir + '/' + class_ + '/' +
                                                 images[class_][cursors[class_]])
-                preprocessed = self.preprocess_image(absolute_path)
+                preprocessed = self.preprocess_image(absolute_path, rescale)
                 data = np.array([preprocessed])
                 label = np.array([np.zeros(len(classes))])
                 label[0][order.index(class_)] = 1
@@ -58,4 +60,4 @@ class ImagePreprocessor:
                     cursors[class_] = 0
                 else:
                     cursors[class_] += 1
-                yield data, label
+                yield step + 1, data, label
