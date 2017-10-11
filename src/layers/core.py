3675928a03cf60aaa882dbb60e4edb49c9d3004f
MarxSoul55 <wjacademic@gmail.com>
2017-10-10 17:47:14 -0700

    Changed weight-init to orthogonal.

--- src/layers/core.py
+++ src/layers/core.py
@@ -6,8 +6,6 @@ import tensorflow as tf
 def dense(input_, output_col):
     """
     A fully-connected layer of neurons.
-    Initializes weights from a normal distribution with mean 0 and STD 0.01.
-    A bias-tensor (initialized to 0) is added to the resulting tensor.
 
     # Parameters
         input_ (tensor): A rank-2 tensor of shape [samples, columns].
@@ -16,6 +14,8 @@ def dense(input_, output_col):
         A tensor.
     """
     input_col = input_.shape.as_list()[1]
-    weight = tf.Variable(tf.random_normal([input_col, output_col], mean=0.0, stddev=0.01))
+    weight_shape = [input_col, output_col]
+    initializer = tf.orthogonal_initializer(gain=1.0, dtype=tf.float32)
+    weight = tf.Variable(initializer(weight_shape, dtype=tf.float32))
     bias = tf.Variable(tf.constant(0.0, shape=[output_col]))
     return tf.matmul(input_, weight) + bias
