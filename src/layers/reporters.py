b713009b45f9f5a555c061781b5c3b1da42d8bc6
MarxSoul55 <wjacademic@gmail.com>
2017-10-11 21:17:31 -0700

    Added new reporter for multiclass-labels

--- src/layers/reporters.py
+++ src/layers/reporters.py
@@ -3,9 +3,26 @@
 import tensorflow as tf
 
 
-def accuracy_reporter(labels, output):
+def binary_accuracy_reporter(labels, output):
     """
-    Generates a tensor for reporting accuracy.
+    Generates a tensor for reporting accuracy. Assumes multi-class labels.
+
+    # Parameters
+        labels (tensor): Tensor for holding labels.
+        output (tensor): Output of the model.
+    # Returns
+        An accuracy-tensor.
+    """
+    bools_labels = tf.cast(labels, tf.bool)
+    halves = tf.constant(0.5, dtype=tf.float32, shape=output.shape.as_list())
+    bools_output = tf.greater_equal(output, halves)
+    bools_total = tf.equal(bools_labels, bools_output)
+    return tf.reduce_mean(tf.cast(bools_total, tf.float32))
+
+
+def categorical_accuracy_reporter(labels, output):
+    """
+    Generates a tensor for reporting accuracy. Assumes one-class-only labels.
 
     # Parameters
         labels (tensor): Tensor for holding labels.
@@ -23,7 +40,7 @@ def report(step, total_steps, current_accuracy, current_objective):
 
     # Parameters
         step (int): Current step.
-        total_steps (int): Total amount of steps to take.
+        total_steps (int): Total amount of steps that are being taken.
         current_accuracy (float): Current accuracy to be reported.
         current_objective (float): Current objective to be reported.
     """
