21d36f55af0bd77f80386040eebdf1f38efd66f6
MarxSoul55 <wjacademic@gmail.com>
2017-10-24 11:01:58 -0700

    Edited docstrings to include details of activation-functions.

--- src/layers/activations.py
+++ src/layers/activations.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 def crelu(input_):
     """
-    Applies the concatenated rectified-linear-unit (CReLU) on the input, element-wise.
+    Applies the concatenated rectified-linear-unit (CReLU) on the input, element-wise:
+        return [max(0, x), max(0, -x)]
 
     # Parameters
         input_ (tensor): The tensor to be CReLU'd.
@@ -17,7 +18,11 @@ def crelu(input_):
 
 def elu(input_):
     """
-    Applies the exponential-linear-unit (ELU) on the input, element-wise.
+    Applies the exponential-linear-unit (ELU) on the input, element-wise:
+        if x < 0:
+            return exp(x) - 1
+        else:
+            return x
 
     # Parameters
         input_ (tensor): The tensor to be ELU'd.
@@ -29,25 +34,31 @@ def elu(input_):
 
 def prelu(input_, looks_linear=False):
     """
-    Applies the parametric linear-unit (PReLU) on the input, element-wise.
+    Applies the parametric linear-unit (PReLU) on the input, element-wise:
+        if x < 0:
+            return variable * x
+        else:
+            return x
 
     # Parameters
         input_ (tensor): The tensor to be PReLU'd.
-        looks_linear (bool): Whether `alpha` is initialized to 1 or not.
-                             See https://arxiv.org/pdf/1702.08591.pdf for details.
+        looks_linear (bool): Whether `alpha` is initialized to 1, or 0.25 (as original paper).
+                             See https://arxiv.org/pdf/1702.08591.pdf for details on `True`.
     # Returns
         The resulting tensor.
     """
+    alpha_shape = input_.shape.as_list()[1:]
     if looks_linear:
-        alpha = tf.Variable(tf.constant(1, dtype=tf.float32, shape=input_.shape.as_list()[-1]))
+        alpha = tf.Variable(tf.constant(1, dtype=tf.float32, shape=alpha_shape))
     else:
-        alpha = tf.Variable(tf.constant(0, dtype=tf.float32, shape=input_.shape.as_list()[-1]))
+        alpha = tf.Variable(tf.constant(0.25, dtype=tf.float32, shape=alpha_shape))
     return tf.maximum(0.0, input_) + alpha * tf.minimum(0.0, input_)
 
 
 def relu(input_):
     """
-    Applies rectified-linear-unit (ReLU) on the input, element-wise.
+    Applies rectified-linear-unit (ReLU) on the input, element-wise:
+        return max(0, x)
 
     # Parameters
         input_ (tensor): The tensor to be ReLU'd.
@@ -59,7 +70,8 @@ def relu(input_):
 
 def sigmoid(input_):
     """
-    Applies sigmoid function to the input, element-wise.
+    Applies sigmoid function to the input, element-wise:
+        return 1 / (1 + exp(-x))
 
     # Parameters
         input_ (tensor): The tensor to be sigmoid'd.
@@ -72,6 +84,7 @@ def sigmoid(input_):
 def softmax(input_):
     """
     Applies softmax function to the input.
+        return e^x / sum(e^x)
 
     # Parameters
         input_ (tensor): The tensor to be softmax'd.
@@ -84,6 +97,7 @@ def softmax(input_):
 def tanh(input_):
     """
     Applies hyperbolic-tangent to the input.
+        return tanh(x)
 
     # Parameters
         input_ (tensor): The tensor to be tanh'd.
