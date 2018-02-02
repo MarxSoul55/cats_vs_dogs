b2e96034ffcf135fe38e45ebeb3a7971aa91220f
MarxSoul55 <wjacademic@gmail.com>
2018-02-02 15:03:21 -0800

    Cleaning up some old files

--- src/architecture.py
+++ src/architecture.py
@@ -1,50 +1,20 @@
-"""Defines the architecture of the model and functions for STNs."""
+"""Defines the architecture of the model via a genetic algorithm."""
 
 import tensorflow as tf
 
-from licensed.transformer import spatial_transformer
 
-
-def localization_network(input_):
+def convolution_elu(input_, filters):
     """
-    Given a tensor, apply an affine transformation and give the result.
-    Assumes `input_` is a square tensor of shape [a, b, b, c].
+    Bootstrapping function for the genetic algorithm.
 
     # Parameters
-        input_ (tensor): The input.
+        input_ (tensor): The tensor that will serve as the input.
+        filters (int): However many filters the layer will have.
     # Returns
-        A composite, TensorFlow operation.
+        The ELU'd result of the convolution.
     """
-    if input_.shape.as_list()[1] != input_.shape.as_list()[2]:
-        raise IndexError('Expected a square-like input, was disappointed.')
-    ln0 = tf.layers.conv2d(input_, 8, 3, padding='same',
-                           kernel_initializer=tf.orthogonal_initializer())
-    ln1 = tf.nn.elu(ln0)
-    ln2 = tf.layers.max_pooling2d(ln1, 2, 2)
-    ln3 = tf.layers.conv2d(ln2, 16, 3, padding='same',
-                           kernel_initializer=tf.orthogonal_initializer())
-    ln4 = tf.nn.elu(ln3)
-    ln5 = tf.layers.max_pooling2d(ln4, 2, 2)
-    ln6 = tf.reshape(ln5, [1, (ln5.shape.as_list()[1] ** 2) * ln5.shape.as_list()[3]])
-    ln7 = tf.layers.dense(ln6, 6)
-    ln7_rows, ln7_cols = ln7.shape.as_list()
-    w_ln8 = tf.Variable(tf.zeros([ln7_rows, 6]))
-    b_ln8 = tf.Variable(initial_value=[[1, 0, 0], [0, 1, 0]], dtype=tf.float32)
-    ln8 = tf.matmul(tf.zeros([ln7_rows, ln7_cols]), w_ln8) + b_ln8
-    return ln8
-
-
-def spatial_transformer_network(input_):
-    """
-    Interface for spatial-transformer networks.
-
-    # Parameters
-        input_ (tensor): Tensor that was inputted into the localization network.
-    # Returns
-        A TensorFlow operation.
-    """
-    transformation_matrix = localization_network(input_)
-    return spatial_transformer(input_, transformation_matrix)
+    return tf.nn.elu(tf.layers.conv2d(input_, filters, 3, padding='same',
+                                      kernel_initializer=tf.orthogonal_initializer()))
 
 
 def model(input_):
