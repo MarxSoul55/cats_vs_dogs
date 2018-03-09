552f615842f5ec5fa991ed8e652376ca212bd9b8
MarxSoul55 <wjacademic@gmail.com>
2018-03-09 15:31:47 -0800

    added constants for the saved model's dir and the tensorboard dir

--- src/constants.py
+++ src/constants.py
@@ -10,3 +10,7 @@ CHAN = 3
 ENCODING = {'cats': [1, 0], 'dogs': [0, 1]}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_directory`.
 TRAIN_DIR = 'data/train'
+# This directory will hold saved data about the model.
+SAVEMODEL_DIR = 'saved/model'
+# This directory will hold tensorboard files.
+TENSORBOARD_DIR = 'tensorboard'
