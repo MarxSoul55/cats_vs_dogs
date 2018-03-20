0b4c263c4a97173b0cddff6da9d5e06b3bd9bf95
MarxSoul55 <wjacademic@gmail.com>
2018-03-20 11:36:08 -0700

    The `SAVEMODEL_DIR` constant doesn't operate how I thought it would... so I updated the comments.

--- src/constants.py
+++ src/constants.py
@@ -13,7 +13,8 @@ ENCODING = {'cats': [1, 0], 'dogs': [0, 1]}
 LABEL_SHAPE = [1, 2]
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_directory`.
 TRAIN_DIR = 'data/train'
-# This directory will hold saved data about the model.
+# X/Y : X is the name of the directory that will hold saved data about the model.
+# Y is the prefix for the .data, .index, and .meta files.
 SAVEMODEL_DIR = 'saved/model'
 # This directory will hold tensorboard files.
 TENSORBOARD_DIR = 'tensorboard'
