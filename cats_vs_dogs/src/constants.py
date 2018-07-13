fc517e464d8f81ea1bbf4101ce80d6d472104a88
Wes <wjacademic@gmail.com>
2018-07-12 18:53:50 -0700

    Fixed incorrect method reference.

--- cats_vs_dogs/src/constants.py
+++ cats_vs_dogs/src/constants.py
@@ -4,12 +4,12 @@
 IN_SHAPE = [1, 128, 128, 3]
 # Color space that the input-image will be converted to.
 COLOR_SPACE = 'CIELAB'
-# Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_directory`.
+# Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 # WARNING: ANY CHANGE TO THE LABELS MUST BE ACCOUNTED FOR IN `LABEL_SHAPE` BELOW!
 ENCODING = {'cats': [1, 0], 'dogs': [0, 1]}
 # Shape of a label as shown above in `encoding`.
 LABEL_SHAPE = [1, 2]
-# Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_directory`.
+# Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 TRAIN_DIR = 'data/train'
 # X/Y : X is the name of the directory that will hold saved data about the model.
 # Y is the prefix for the .data, .index, and .meta files.
