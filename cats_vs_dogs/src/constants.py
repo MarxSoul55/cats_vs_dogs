2c5b5796f7291a2877425ca32592c076cd9dcbe0
Wes <wjacademic@gmail.com>
2018-07-22 21:42:38 -0700

    Clarified `encoding` constant.

--- cats_vs_dogs/src/constants.py
+++ cats_vs_dogs/src/constants.py
@@ -7,7 +7,8 @@ IN_SHAPE = [1, 128, 128, 3]
 # Color space that the input-image will be converted to.
 COLORSPACE = 'CIELAB'
 # Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
-# ALL LABELS MUST BE RANK 1 TENSORS!
+# ALL LABELS MUST BE RANK 1 TENSORS! (i.e. shape=[1, X] where X is some integer)
+# For obvious reasons, the labels' shapes must also match the model's output shape.
 ENCODING = {'cats': np.array([[1, 0]]), 'dogs': np.array([[0, 1]])}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 TRAIN_DIR = '../data/train'
