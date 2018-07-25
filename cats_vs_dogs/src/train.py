7079546841cbd15e01d7902ccce1d2444a1f885c
Wes <wjacademic@gmail.com>
2018-07-25 10:46:59 -0700

    Removed class, will try functional approach.

--- cats_vs_dogs/src/train.py
+++ cats_vs_dogs/src/train.py
@@ -10,85 +10,73 @@ from model import architecture
 from preprocessing.pipelines import ImageDataPipeline
 
 
-class TrainingPipeline(ImageDataPipeline):
+def clear_tensorboard(path):
+    """
+    Clears the directory for tensorboard, if it exists.
 
-    def __init__(self,
-                 rescale,
-                 colorspace,
-                 current_bounds,
-                 desired_bounds,
-                 dtype='float32'):
-        """
-        TODO
-        """
-        self.sess = tf.Session()
-        ImageDataPipeline.__init__(self, rescale, colorspace, current_bounds, desired_bounds,
-                                   dtype)
+    Parameters:
+        - path (str)
+            - Path to the directory.
+    """
+    if os.path.isdir(path):
+        shutil.rmtree(path)
 
-    def clear_tensorboard(self,
-                          path):
-        """
-        Clears the directory for tensorboard, if it exists.
 
-        Parameters:
-            - path (str)
-                - Path to the directory.
-        """
-        if os.path.isdir(path):
-            shutil.rmtree(path)
+def build_graph(sess,
+                input_shape,
+                label_shape):
+    """
+    Builds a graph for the training process.
 
-    def build_graph(self,
-                    input_shape,
-                    label_shape):
-        """
-        Builds a graph for the training process.
+    Parameters:
+        - sess (tf.Session)
+            - A session object where the graph will be loaded.
+        - input_shape (list)
+            - Shape of the input tensor.
+        - label_shape (list)
+            - Shape of the label tensor.
+    Returns:
+        - An input placeholder.
+        - A label placeholder.
+        - An optimizer.minimize method from TensorFlow.
+    """
+    input_ = tf.placeholder(tf.float32, shape=input_shape, name='input')
+    output = architecture.primary(input_, name='model')
+    sess.run(tf.global_variables_initializer())
+    label = tf.placeholder(tf.float32, shape=label_shape)
+    objective = tf.sqrt(tf.reduce_mean(tf.squared_difference(label, output)), name='objective')
+    optimizer = tf.train.MomentumOptimizer(0.0001, 0.9).minimize(objective, name='optimizer')
+    tf.summary.scalar('objective_summary', objective)
+    return input_, label, optimizer
 
-        Parameters:
-            - input_shape (list)
-                - Shape of the input tensor.
-            - label_shape (list)
-                - Shape of the label tensor.
-        Returns:
-            - An input placeholder.
-            - A label placeholder.
-            - An optimizer.minimize method from TensorFlow.
-        """
-        input_ = tf.placeholder(tf.float32, shape=input_shape, name='input')
-        output = architecture.primary(input_, name='model')
-        self.sess.run(tf.global_variables_initializer())
-        label = tf.placeholder(tf.float32, shape=label_shape)
-        objective = tf.sqrt(tf.reduce_mean(tf.squared_difference(label, output)), name='objective')
-        optimizer = tf.train.MomentumOptimizer(0.0001, 0.9).minimize(objective, name='optimizer')
-        tf.summary.scalar('objective_summary', objective)
-        return input_, label, optimizer
 
-    def load_graph(self,
-                   savemodel_dir):
-        """
-        Loads an existing graph from a directory saved to by a tf.train.Saver object.
+def load_graph(sess,
+               savemodel_dir):
+    """
+    Loads an existing graph from a directory saved to by a tf.train.Saver object.
 
-        Parameters:
-            - savemodel_dir (str)
-                - X/Y
-                    - X is the name of the directory that holds saved data about the model.
-                    - Y is the prefix for the .data, .index, and .meta files.
-        """
-        pass
+    Parameters:
+        - savemodel_dir (str)
+            - X/Y
+                - X is the name of the directory that holds saved data about the model.
+                - Y is the prefix for the .data, .index, and .meta files.
+    """
+    pass
 
-    def initialize_summary_nodes(self,
-                                 tensorboard_dir):
-        """
-        Merges all summary ops on graph to a single op, and initializes a FileWriter object.
 
-        Parameters:
-            - tensorboard_dir (str)
-                - Path to the directory where the FileWriter object will store summaries.
-        Returns:
-            - summary (a tf operation)
-                - A node on the graph which collects the specified data.
-            - writer (tf.summary.FileWriter)
-                - Object which saves summaries to a directory.
-        """
-        summary = tf.summary.merge_all()
-        writer = tf.summary.FileWriter(tensorboard_dir, graph=self.sess.graph)
-        return summary, writer
+def initialize_summary_nodes(tensorboard_dir):
+    """
+    Merges all summary ops on graph to a single op, and initializes a FileWriter object.
+
+    Parameters:
+        - tensorboard_dir (str)
+            - Path to the directory where the FileWriter object will store summaries.
+    Returns:
+        - summary (a tf operation)
+            - A node on the graph which collects the specified data.
+        - writer (tf.summary.FileWriter)
+            - Object which saves summaries to a directory.
+    """
+    summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter(tensorboard_dir, graph=self.sess.graph)
+    return summary, writer
