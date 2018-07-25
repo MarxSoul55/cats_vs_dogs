c07070a7407ec34414948dd6fa4a63059f16b69b
Wes <wjacademic@gmail.com>
2018-07-24 18:31:02 -0700

    Added template prototype for class implementation.

--- cats_vs_dogs/src/train.py
+++ cats_vs_dogs/src/train.py
@@ -10,56 +10,78 @@ from model import architecture
 from preprocessing.pipelines import ImageDataPipeline
 
 
-def clear_tensorboard(path):
-    """
-    Clears the directory for tensorboard, if it exists.
+class TrainingPipeline(ImageDataPipeline):
 
-    Parameters:
-        - path (str)
-            - Path to the directory.
-    """
-    if os.path.isdir(path):
-        shutil.rmtree(path)
+    def __init__(self,
+                 rescale,
+                 colorspace,
+                 current_bounds,
+                 desired_bounds,
+                 dtype='float32'):
+        """
+        TODO
+        """
+        self.sess = tf.Session()
+        ImageDataPipeline.__init__(self, rescale, colorspace, current_bounds, desired_bounds,
+                                   dtype)
 
+    def clear_tensorboard(self,
+                          path):
+        """
+        Clears the directory for tensorboard, if it exists.
 
-def train(steps,
-          resuming):
-    """
-    Builds up a graph of operations from scratch to train a model.
+        Parameters:
+            - path (str)
+                - Path to the directory.
+        """
+        if os.path.isdir(path):
+            shutil.rmtree(path)
 
-    Parameters:
-        - steps (int)
-            - Number of gradient updates to perform.
-        - resuming (bool)
-            - Whether to train from scratch or resume training from a saved model.
-    """
-    clear_tensorboard(c.TENSORBOARD_DIR)
-    sess = tf.Session()
-    if resuming:
-        loader = tf.train.import_meta_graph(c.SAVEMODEL_DIR + '.meta')
-        loader.restore(sess, c.SAVEMODEL_DIR)
-        input_ = sess.graph.get_tensor_by_name('input:0')
-        label = sess.graph.get_tensor_by_name('label:0')
-        optimizer = sess.graph.get_operation_by_name('optimizer')
-    else:
+    def build_graph(self):
+        """
+        Builds a graph for the training process.
+
+        Returns:
+            - An input placeholder.
+            - A label placeholder.
+            - An optimizer.minimize method from TensorFlow.
+        """
         input_ = tf.placeholder(tf.float32, shape=c.IN_SHAPE, name='input')
         output = architecture.primary(input_, name='model')
-        sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.global_variables_initializer())
         label = tf.placeholder(tf.float32, shape=list(c.ENCODING.values())[0].shape)
         objective = tf.sqrt(tf.reduce_mean(tf.squared_difference(label, output)), name='objective')
         optimizer = tf.train.MomentumOptimizer(0.0001, 0.9).minimize(objective, name='optimizer')
         tf.summary.scalar('objective_summary', objective)
-    summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(c.TENSORBOARD_DIR, graph=sess.graph)
-    preprocessor = ImageDataPipeline(c.IN_SHAPE[1:3],
-                                     c.COLORSPACE,
-                                     [[0, 255], [0, 255], [0, 255]],
-                                     [[0, 1], [-1, 1], [-1, 1]])
-    for step, input_arg, label_arg in preprocessor.preprocess_classes(steps, c.TRAIN_DIR,
-                                                                      c.ENCODING):
-        print('Step: {}/{}'.format(step, steps))
-        _, step_summary = sess.run([optimizer, summary],
-                                   feed_dict={input_: input_arg, label: label_arg})
-        writer.add_summary(step_summary, global_step=step)
-    tf.train.Saver().save(sess, c.SAVEMODEL_DIR)
-    print('\a')
+        return input_, label, optimizer
+
+    def load_graph(self,
+                   savemodel_dir):
+        """
+        Loads an existing graph from a directory saved to by a tf.train.Saver object.
+
+        Parameters:
+            - savemodel_dir (str)
+                - X/Y
+                    - X is the name of the directory that holds saved data about the model.
+                    - Y is the prefix for the .data, .index, and .meta files.
+        """
+        pass
+
+    def initialize_summary_nodes(self,
+                                 tensorboard_dir):
+        """
+        Merges all summary ops on graph to a single op, and initializes a FileWriter object.
+
+        Parameters:
+            - tensorboard_dir (str)
+                - Path to the directory where the FileWriter object will store summaries.
+        Returns:
+            - summary (a tf operation)
+                - A node on the graph which collects the specified data.
+            - writer (tf.summary.FileWriter)
+                - Object which saves summaries to a directory.
+        """
+        summary = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(tensorboard_dir, graph=self.sess.graph)
+        return summary, writer
