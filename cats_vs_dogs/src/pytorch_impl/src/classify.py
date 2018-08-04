aa8a4162228660272112288e208e393585d31d58
Wes <wjacademic@gmail.com>
2018-08-04 12:37:48 -0700

    Moved inner function out; removed unnecessary instance args for preprocessor

--- cats_vs_dogs/src/pytorch_impl/src/classify.py
+++ cats_vs_dogs/src/pytorch_impl/src/classify.py
@@ -9,6 +9,29 @@ import constants as c
 from model.pipelines import ImageDataPipeline
 
 
+def predicted_label(prediction_tensor,
+                    encoding):
+    """
+    Generates the predicted label by comparing the tensor prediction to `encoding`.
+
+    Parameters:
+        - prediction (np.ndarray)
+            - The prediction as represented by the model's tensor output.
+        - encoding (dict, str --> np.ndarray)
+            - See the parent function for details.
+    Returns:
+        - A string; the predicted label.
+    """
+    classes = list(encoding.keys())
+    labels = list(encoding.values())
+    differences = []
+    for label in labels:
+        l2_difference = np.sqrt(np.sum((label - prediction_tensor) ** 2))
+        differences.append(l2_difference)
+    index_of_smallest_difference = differences.index(min(differences))
+    return classes[index_of_smallest_difference]
+
+
 def classify(path,
              encoding):
     """
@@ -31,36 +54,12 @@ def classify(path,
         - If given path to an image file on disk, returns a string that is either 'cat' or 'dog'.
         - If given path to a directory, returns a dictionary {'filename': 'guessed animal'}
     """
-    def predicted_label(prediction_tensor,
-                        encoding):
-        """
-        Generates the predicted label by comparing the tensor prediction to `encoding`.
-
-        Parameters:
-            - prediction (np.ndarray)
-                - The prediction as represented by the model's tensor output.
-            - encoding (dict, str --> np.ndarray)
-                - See the parent function for details.
-        Returns:
-            - A string; the predicted label.
-        """
-        classes = list(encoding.keys())
-        labels = list(encoding.values())
-        differences = []
-        for label in labels:
-            l2_difference = np.sqrt(np.sum((label - prediction_tensor) ** 2))
-            differences.append(l2_difference)
-        index_of_smallest_difference = differences.index(min(differences))
-        return classes[index_of_smallest_difference]
     sess = tf.Session()
     loader = tf.train.import_meta_graph(c.SAVEMODEL_DIR + '.meta')
     loader.restore(sess, c.SAVEMODEL_DIR)
     input_ = sess.graph.get_tensor_by_name('input:0')
     model = sess.graph.get_tensor_by_name('model/output/output:0')
-    preprocessor = ImageDataPipeline(c.IN_SHAPE[1:3],
-                                     c.COLORSPACE,
-                                     [[0, 255], [0, 255], [0, 255]],
-                                     [[0, 1], [-1, 1], [-1, 1]])
+    preprocessor = ImageDataPipeline()
     if os.path.isdir(path):
         results = {}
         for image_name, preprocessed_image in preprocessor.preprocess_directory(path):
