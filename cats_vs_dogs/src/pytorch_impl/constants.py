9e7d28a7e20d71af29e45d027cb1d2f2969b054e
Wes <wjacademic@gmail.com>
2018-08-05 11:36:56 -0700

    Corrected path to local save dir.

--- cats_vs_dogs/src/pytorch_impl/constants.py
+++ cats_vs_dogs/src/pytorch_impl/constants.py
@@ -9,4 +9,4 @@ ENCODING = {'cat': np.array([[1, 0]]), 'dog': np.array([[0, 1]])}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 TRAIN_DIR = 'cats_vs_dogs/data/train'
 # Path to the saved model.
-SAVEPATH = 'cats_vs_dogs/bin/saved/model.pth'
+SAVEPATH = 'cats_vs_dogs/bin/pytorch_impl/saved/model.pth'
