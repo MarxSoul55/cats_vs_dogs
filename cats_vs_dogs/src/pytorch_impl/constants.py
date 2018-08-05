cb524e4899973f3e0d06c12205908eed094a6d93
Wes <wjacademic@gmail.com>
2018-08-04 18:58:20 -0700

    Label won't be plural.

--- cats_vs_dogs/src/pytorch_impl/constants.py
+++ cats_vs_dogs/src/pytorch_impl/constants.py
@@ -5,7 +5,7 @@ import numpy as np
 # Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 # ALL LABELS MUST BE RANK 1 TENSORS! (i.e. shape=[1, X] where X is some integer)
 # For obvious reasons, the labels' shapes must also match the model's output shape.
-ENCODING = {'cats': np.array([[1, 0]]), 'dogs': np.array([[0, 1]])}
+ENCODING = {'cat': np.array([[1, 0]]), 'dog': np.array([[0, 1]])}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 TRAIN_DIR = 'cats_vs_dogs/data/train'
 # Path to the saved model.
