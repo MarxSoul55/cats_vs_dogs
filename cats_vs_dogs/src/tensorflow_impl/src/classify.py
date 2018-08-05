705cab8fe7bb46f157872af0e3f0cc44df7b3ec2
Wes <wjacademic@gmail.com>
2018-08-05 11:38:16 -0700

    Converted to correct tensor format.

--- cats_vs_dogs/src/tensorflow_impl/src/classify.py
+++ cats_vs_dogs/src/tensorflow_impl/src/classify.py
@@ -57,10 +57,7 @@ def classify(path,
     loader.restore(sess, c.SAVEMODEL_DIR)
     input_ = sess.graph.get_tensor_by_name('input:0')
     model = sess.graph.get_tensor_by_name('model/output/output:0')
-    preprocessor = ImageDataPipeline(c.IN_SHAPE[1:3],
-                                     c.COLORSPACE,
-                                     [[0, 255], [0, 255], [0, 255]],
-                                     [[0, 1], [-1, 1], [-1, 1]])
+    preprocessor = ImageDataPipeline(mode='NHWC')
     if os.path.isdir(path):
         results = {}
         for image_name, preprocessed_image in preprocessor.preprocess_directory(path):
