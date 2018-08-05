66ad6b46664945c46f728c27b0159495078cc151
Wes <wjacademic@gmail.com>
2018-08-05 12:15:54 -0700

    Added imports for tensorflow impl

--- cats_vs_dogs/__main__.py
+++ cats_vs_dogs/__main__.py
@@ -7,6 +7,9 @@ import sys
 from src.pytorch_impl import constants as pyt_constants
 from src.pytorch_impl.src import classify as pyt_classify
 from src.pytorch_impl.src import train as pyt_train
+from src.tensorflow_impl import constants as tf_constants
+from src.tensorflow_impl.src import classify as tf_classify
+from src.tensorflow_impl.src import train as tf_train
 
 
 def training_prompt():
