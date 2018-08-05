71ab58a4460146cc0846d3925ebb64fd73baa3b8
Wes <wjacademic@gmail.com>
2018-08-05 12:15:04 -0700

    Fixed constants to reflect new dir structure.

--- cats_vs_dogs/src/tensorflow_impl/constants.py
+++ cats_vs_dogs/src/tensorflow_impl/constants.py
@@ -2,18 +2,14 @@
 
 import numpy as np
 
-# Shape of the input in NHWC format.
-IN_SHAPE = [1, 128, 128, 3]
-# Color space that the input-image will be converted to.
-COLORSPACE = 'CIELAB'
 # Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 # ALL LABELS MUST BE RANK 1 TENSORS! (i.e. shape=[1, X] where X is some integer)
 # For obvious reasons, the labels' shapes must also match the model's output shape.
 ENCODING = {'cat': np.array([[1, 0]]), 'dog': np.array([[0, 1]])}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
-TRAIN_DIR = '../data/train'
+TRAIN_DIR = 'cats_vs_dogs/data/train'
 # X/Y : X is the name of the directory that will hold saved data about the model.
 # Y is the prefix for the .data, .index, and .meta files.
-SAVEMODEL_DIR = '../bin/saved/model'
+SAVEPATH = 'cats_vs_dogs/bin/tensorflow_impl/saved/model'
 # This directory will hold tensorboard files.
-TENSORBOARD_DIR = '../bin/tensorboard'
+TENSORBOARD_DIR = 'cats_vs_dogs/bin/tensorflow_impl/tensorboard'
