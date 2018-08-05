6aad5bfcd07d247ae7d9abccbe89dc514fd78897
Wes <wjacademic@gmail.com>
2018-08-05 11:32:59 -0700

    Refactored training module.

--- cats_vs_dogs/src/tensorflow_impl/src/train.py
+++ cats_vs_dogs/src/tensorflow_impl/src/train.py
@@ -5,61 +5,58 @@ import shutil
 
 import tensorflow as tf
 
-import constants as c
-from model import architecture
-from preprocessing.pipelines import ImageDataPipeline
+from .model import architecture
+from .preprocessing import ImageDataPipeline
 
 
-def clear_tensorboard(path):
-    """
-    Clears the directory for tensorboard, if it exists.
-
-    Parameters:
-        - path (str)
-            - Path to the directory.
-    """
-    if os.path.isdir(path):
-        shutil.rmtree(path)
-
-
-def train(steps,
-          resuming):
+def train(train_dir,
+          encoding,
+          steps,
+          savepath,
+          tensorboard_dir,
+          resuming=True):
     """
     Builds up a graph of operations from scratch to train a model.
 
     Parameters:
+        - train_dir, encoding
+            - Parameters for the preprocessor.
+            - See `preprocessing.ImageDataPipeline.preprocess_classes` for details.
         - steps (int)
-            - Number of gradient updates to perform.
+            - Number of gradient updates (samples to train on).
+        - savepath (str)
+            - Path where the model will be saved/loaded from.
+            - e.g. X/Y will give: X/Y.meta, X/Y.index, etc. for other tf.train.Saver artifacts.
+        - tensorboard_dir (str)
+            - Directory where the tensorboard files are stored.
         - resuming (bool)
-            - Whether to train from scratch or resume training from a saved model.
+            - Whether to resume training from a saved model or to start from scratch.
     """
-    clear_tensorboard(c.TENSORBOARD_DIR)
+    if os.path.isdir(tensorboard_dir):
+        shutil.rmtree(tensorboard_dir)
     sess = tf.Session()
     if resuming:
-        loader = tf.train.import_meta_graph(c.SAVEMODEL_DIR + '.meta')
-        loader.restore(sess, c.SAVEMODEL_DIR)
+        loader = tf.train.import_meta_graph(savepath + '.meta')
+        loader.restore(sess, savepath)
         input_ = sess.graph.get_tensor_by_name('input:0')
         label = sess.graph.get_tensor_by_name('label:0')
         optimizer = sess.graph.get_operation_by_name('optimizer')
     else:
-        input_ = tf.placeholder(tf.float32, shape=c.IN_SHAPE, name='input')
-        output = architecture.model(input_, name='model')
+        input_ = tf.placeholder(tf.float32, name='input')
+        output = architecture.baby_resnet(input_, name='model')
         sess.run(tf.global_variables_initializer())
-        label = tf.placeholder(tf.float32, shape=list(c.ENCODING.values())[0].shape)
+        label = tf.placeholder(tf.float32, name='label')
         objective = tf.sqrt(tf.reduce_mean(tf.squared_difference(label, output)), name='objective')
-        optimizer = tf.train.MomentumOptimizer(0.0001, 0.9).minimize(objective, name='optimizer')
+        optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(objective, name='optimizer')
         tf.summary.scalar('objective_summary', objective)
     summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(c.TENSORBOARD_DIR, graph=sess.graph)
-    preprocessor = ImageDataPipeline(c.IN_SHAPE[1:3],
-                                     c.COLORSPACE,
-                                     [[0, 255], [0, 255], [0, 255]],
-                                     [[0, 1], [-1, 1], [-1, 1]])
-    for step, input_arg, label_arg in preprocessor.preprocess_classes(steps, c.TRAIN_DIR,
-                                                                      c.ENCODING):
-        print('Step: {}/{}'.format(step, steps))
+    writer = tf.summary.FileWriter(tensorboard_dir, graph=sess.graph)
+    preprocessor = ImageDataPipeline()
+    for step, img_path, img_tensor, img_label in preprocessor.preprocess_classes(steps, train_dir,
+                                                                                 encoding):
+        print('Step: {} | Image: {}'.format(step, img_path))
         _, step_summary = sess.run([optimizer, summary],
-                                   feed_dict={input_: input_arg, label: label_arg})
+                                   feed_dict={input_: img_tensor, label: img_label})
         writer.add_summary(step_summary, global_step=step)
-    tf.train.Saver().save(sess, c.SAVEMODEL_DIR)
+    tf.train.Saver().save(sess, savepath)
     print('\a')
