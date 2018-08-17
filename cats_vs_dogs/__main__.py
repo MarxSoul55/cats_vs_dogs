e89f75a6e98b54df1fabba38193141044542d63d
Wes <wjacademic@gmail.com>
2018-08-16 17:10:15 -0700

    Added startup question on whether to use saved config.

--- cats_vs_dogs/__main__.py
+++ cats_vs_dogs/__main__.py
@@ -2,36 +2,19 @@
 
 import argparse
 import msvcrt
+import os
 import pickle
 import sys
 
 from PyInquirer import prompt
 
+import questions
 from src.pytorch_impl import constants as pyt_constants
 from src.pytorch_impl.src import classify as pyt_classify
 from src.pytorch_impl.src import train as pyt_train
 from src.tensorflow_impl import constants as tf_constants
 from src.tensorflow_impl.src import classify as tf_classify
 from src.tensorflow_impl.src import train as tf_train
-
-
-def yes_or_no(message):
-    """
-    Prompts the user with a custom yes-or-no prompt.
-
-    Parameters:
-        - message (str)
-            - Message to be printed to the user.
-    Returns:
-        - A string; either 'y' or 'n'.
-    """
-    print(message)
-    while True:
-        resp = msvcrt.getch().decode().lower()
-        if resp == 'y' or resp == 'n':
-            return resp
-        else:
-            print('Press either the Y or N key.')
 
 
 def save_config(train_dir,
@@ -75,21 +58,20 @@ def load_config():
 
 
 def main():
-    operation_question = [
-        {
-            'type': 'list',
-            'name': 'selected_operation',
-            'message': 'Select operation to perform:',
-            'choices': [
-                'Train model on dataset.',
-                'Test model on testset.',
-                'Classify an image or directory of images.'
-            ]
-        }
-    ]
-    selected_operation = prompt(operation_question)['selected_operation']
+    if 'config.pkl' in os.listdir():
+        use_saved_config = prompt(questions.CONFIGURATION_QUESTION)['use_saved_config']
+        if use_saved_config:
+            config = load_config()
+        else:
+            config = {}
+    else:
+        config = {}
+    selected_operation = prompt(questions.OPERATION_QUESTION)['selected_operation']
     if selected_operation == 'Train model on dataset.':
-        pass
+        if len(config) == 0:
+            train_dir = prompt(questions.TRAIN_DIR_QUESTION)['train_dir']
+            classes = os.listdir(train_dir)
+            pass
     elif selected_operation == 'Test model on testset.':
         pass
     elif selected_operation == 'Classify an image or directory of images.':
