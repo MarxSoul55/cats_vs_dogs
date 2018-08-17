96b7c5277a969bd80311c4f1a3e9bfb70cd3509a
Wes <wjacademic@gmail.com>
2018-08-16 18:04:23 -0700

    Focusing on PyTorch impl for now. Will come back for TF later.

--- cats_vs_dogs/__main__.py
+++ cats_vs_dogs/__main__.py
@@ -12,9 +12,6 @@ import questions
 from src.pytorch_impl import constants as pyt_constants
 from src.pytorch_impl.src import classify as pyt_classify
 from src.pytorch_impl.src import train as pyt_train
-from src.tensorflow_impl import constants as tf_constants
-from src.tensorflow_impl.src import classify as tf_classify
-from src.tensorflow_impl.src import train as tf_train
 
 
 def save_config(train_dir,
