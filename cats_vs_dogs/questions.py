795d0feac7032f48ce4b8486015bb4a2b6933060
Wes <wjacademic@gmail.com>
2018-08-16 18:04:52 -0700

    Added TODO; may want to explain traindir in CLI later.

--- cats_vs_dogs/questions.py
+++ cats_vs_dogs/questions.py
@@ -20,10 +20,11 @@ OPERATION_QUESTION = [
         ]
     }
 ]
-# Does the user want to train? If so—first, find the training directory.
+# User wants to train? Set up all relevant variables.
+# TODO: Add deeper explanation for train_dir in CLI???
 train_dir_message = ('Enter location of dataset; either absolute path or relative to repo root. '
                      '(e.g. X/Y where Y holds class1_dir_pics, class2_dir_pics, etc.)')
-TRAIN_DIR_QUESTION = [
+TRAINING_QUESTIONS = [
     {
         'type': 'input',
         'name': 'train_dir',
