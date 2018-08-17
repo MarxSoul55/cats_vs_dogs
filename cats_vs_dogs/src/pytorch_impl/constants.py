bb4d4afb3f635d2e42c836928de058d346445660
Wes <wjacademic@gmail.com>
2018-08-16 17:43:49 -0700

    Removed outdated constant; this is now a part of the dataset itself.

--- cats_vs_dogs/src/pytorch_impl/constants.py
+++ cats_vs_dogs/src/pytorch_impl/constants.py
@@ -1,11 +1,5 @@
 """Provides interface for frequently used or very important constant values."""
 
-import numpy as np
-
-# Provides `encoding` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
-# ALL LABELS MUST BE RANK 1 TENSORS! (i.e. shape=[1, X] where X is some integer)
-# For obvious reasons, the labels' shapes must also match the model's output shape.
-ENCODING = {'cat': np.array([[1, 0]]), 'dog': np.array([[0, 1]])}
 # Provides `train_dir` argument for `preprocessing.ImagePreprocessor.preprocess_classes`.
 TRAIN_DIR = 'cats_vs_dogs/data/train'
 # Path to the saved model.
