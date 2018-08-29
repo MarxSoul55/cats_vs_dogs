df62591aa48c0f594d5998c9c4d4122a826ea282
Wes <wjacademic@gmail.com>
2018-08-28 17:41:26 -0700

    Will provide link to docs instead.

--- cats_vs_dogs/__main__.py
+++ cats_vs_dogs/__main__.py
@@ -53,27 +53,6 @@ def parse_arguments():
         - A dictionary mapping each argument to the given value.
         - Each string value is lowercased to prevent case sensitivity issues.
     """
-    train_help = '(flag) Tells program to train the model.'
-    resuming_help = ('(flag) Tells program to resume training off of a saved model '
-                     'whose path is given by the --savepath arg.')
-    train_dir_help = ('(str) Tells program where training dataset is; this directory should '
-                      'contain subdirectories (representing each class) of images.')
-    label_dict_path_help = ('(str) This path should point to a .pkl file of a dictionary mapping '
-                            'strings (specifically, the names of the subdirectories in the '
-                            '--train_dir arg) to numpy arrays. The shape of each label should '
-                            'be the same and it should match the output shape of the model.')
-    savepath_help = ('(str) Path that indicates where the model will be saved and loaded from. '
-                     'File extension should be: .pth')
-    tensorboard_dir_help = '(str) Tells program what directory to save tensorboard info in.'
-    steps_help = '(int) Indicates how many images to train on (one gradient update per image).'
-    classify_help = ('(flag) Tells program to classify something using the saved '
-                     'model from the --savepath arg.')
-    source_help = '(str) A path to either an image or directory of images to classify.'
-    implementation_help = ('(str) Either \'pytorch\' (default) or \'tensorflow\'. Note that '
-                           'the TensorFlow implementation uses a `constants` module to hold '
-                           'important paths instead of the CLI args for `train_dir`, '
-                           '`label_dict_path`, and `savepath`. SETTING THESE ARGS FOR THE '
-                           'TENSORFLOW IMPLEMENTATION IS POINTLESS!')
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', action='store_true', help=train_help)
     parser.add_argument('--resuming', action='store_true', help=resuming_help)
